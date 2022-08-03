2c0362fbc77b4e7592a8948a2bc58aef85998041
Juan Manuel Gomez Omil <72764054+JuanchiiGomezZ@users.noreply.github.com>
2022-08-03 15:56:04 -0300

    Add files via upload

--- Javascript/main.js
+++ Javascript/main.js
@@ -3,3 +3,37 @@ window.addEventListener('scroll', function () {
     let windowPosition = window.scrollY > 0;
     header.classList.toggle('scrolling-active', windowPosition);
 })
+
+
+let burgerIcon = document.querySelector(".icon-burger-menu");
+let burgerMenu = document.querySelector(".burger-sections");
+let crossIcon = document.querySelector(".cross-burger-menu");
+
+
+burgerIcon.onclick = () => {
+    burgerMenu.classList.add("active");
+    burgerIcon.classList.add("hide");
+    crossIcon.classList.add("active");
+  };
+  /* Close Menu */
+  crossIcon.onclick = () => {
+    burgerMenu.classList.remove("active");
+    crossIcon.classList.remove("active");
+    burgerIcon.classList.remove("hide");
+  };
+
+
+
+  const typed = new Typed('.typed',{
+    strings: [
+        '<h3 class="subName">Designer</h3>', 
+        '<h3 class="subName">Developer</h3>',
+        '<h3 class="subName">Frontend Developer</h3>'
+    ],
+    typeSpeed:75,
+    starDelay:3000,
+    backSpeed:75,
+    loop:true,
+    backDelay:3000,
+    showCursor:false,
+  });
